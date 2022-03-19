97467354c4fcbb9553e49cd7d095491fd5c5b690
ElpidaHope <paul.oladapo@yahoo.com.com>
2022-03-19 05:30:45 +0100

    remove multiple id in image tag

--- src/components/SliderList.js
+++ src/components/SliderList.js
@@ -4,7 +4,7 @@ const SliderList = ({images, imageNumber, showLightBox}) => {
   return (
     <div className="list-img-container">
             {images.map(image => (
-              <img key={nanoid()} src={image} alt="" id='main-img' className={`list-img-${imageNumber}`} onClick={() => showLightBox()}/>
+              <img key={nanoid()} src={image} alt="" className={`list-img-${imageNumber}`} onClick={() => showLightBox()}/>
             ))}
           </div>
 
